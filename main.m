51f632e1145bcb63f0d47cd517adebf90620bdb6
Chris Swetenham <cswetenham@gmail.com>
2011-10-10 17:14:37 +0100

    properly read in images, convert to double

--- main.m
+++ main.m
@@ -1,5 +1,7 @@
-test_img = imread(strcat('ivr1/data1/', sprintf('%08d', 1), '.jpg'));
-input = zeros(100, size(i,1),size(i,2),size(i,3));
-for i = 3:size(input, 1)
-    input(i-2, :, :, :) = imread(strcat('ivr1/data1/', sprintf('%08d', i), '.jpg'));
+test_img = double(imread(strcat('ivr1/data1/', sprintf('%08d', 1), '.jpg')))/255;
+input = zeros(size(test_img,1), size(test_img,2), size(test_img,3), 100, class(test_img));
+for i = 1:size(input, 4)
+    curimg = imread(strcat('ivr1/data1/', sprintf('%08d', i), '.jpg'));
+    input(:, :, :, i) = double(curimg)/255;
 end
+
