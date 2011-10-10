dd67f8f4aa9dbd179e85b6173e2cc0360390beb6
Chris Swetenham <cswetenham@gmail.com>
2011-10-10 16:49:27 +0100

    what

--- main.m
+++ main.m
@@ -1,1 +1,5 @@
-% asdf
+test_img = imread(strcat('ivr1/data1/', sprintf('%08d', 1), '.jpg'));
+input = zeros(100, size(i,1),size(i,2),size(i,3));
+for i = 3:size(input, 1)
+    input(i-2, :, :, :) = imread(strcat('ivr1/data1/', sprintf('%08d', i), '.jpg'));
+end
