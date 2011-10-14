863d687d929704018120a3428296a1d7700ad9a1
Chris Swetenham <cswetenham@gmail.com>
2011-10-14 18:07:56 +0100

    what is this

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 clear all
 
-ImgData = myreadfolder('data1/', 100);
+ImgData = myreadfolder('data2/', 100);
 
 % compute the average for each pixel
 % average = zeros(size(test_img));
@@ -16,39 +16,60 @@ ImgData = myreadfolder('data1/', 100);
 % med =  median(ImgData(:,:,:,1:10:100), 4);
 % imshow(med);
 
+% difference between each image and the median
 % for i = 1:100
 %     imagesc((ImgData(:,:,:,i) - med)/2 + 0.5);
 % end
 
-% imshow(adapt(ImgData(:,:,:,5), 10, 12));
-filter = fspecial('gaussian', [50 1], 6);
-filter = filter/sum(filter);  % this normalises the filter
+% adaptive thresholding
+Img = ImgData(:,:,:,5);
+NImg = normalize_rgb(Img);
+AdtImg = zeros(size(NImg));
+str = strel(ones(3,3));
+for i = 1:3
+    % AdtImg(:,:,i) = imclose(imopen(adapt(Img(:,:,i), 45, 0.3), str), str);
+    % AdtImg(:,:,i) = adapt(NImg(:,:,i), 45, 0.05);
+end
+% imshow(AdtImg);
+imshow(NImg);
+
+%Test with a single histogram
 edges = zeros(256,1);
 for i = 1 : 256;
-    edges(i) = (i-1)/255.0;
+     edges(i) = (i-1)/255.0;
 end
-  
-for i = 1:100
-    NrmImg = normalize_rgb(ImgData(:,:,:,i));
-    HistR = dohist(NrmImg(:,:,1), 0, edges);
-    HistG = dohist(NrmImg(:,:,2), 0, edges);
-    HistB = dohist(NrmImg(:,:,3), 0, edges);
-    %SmoothHistR = conv(filter,HistR);
-    %SmoothHistG = conv(filter,HistG);
-    %SmoothHistB = conv(filter,HistB);
-    TImg = NrmImg;
-    for j = 1:3
-        TImg(:,:,j) = (NrmImg(:,:,j) > 0.45);
-    end
-    
-    clf
-    subplot(2,1,1);
-    % imshow(NrmImg);
-    imshow(TImg);
-    subplot(2,1,2);
-    plot(edges, HistR, 'r', edges, HistG, 'g', edges, HistB, 'b');
-    axis([0, 1, 0, 1.1*max(HistR)]);
-    pause(0.1);
-end
+HistR = dohist(NImg(:,:,1), 0, edges);
 
+thresholdR = findthresh(HistR, 5/255, 4);
+% filter = fspecial('gaussian', [50 1], 6);
+% filter = filter/sum(filter);  % this normalises the filter
+% edges = zeros(256,1);
+% for i = 1 : 256;
+%     edges(i) = (i-1)/255.0;
+% end
+  
+% for i = 1:100
+%     NrmImg = normalize_rgb(ImgData(:,:,:,i));
+% %     HistR = dohist(NrmImg(:,:,1), 0, edges);
+% %     HistG = dohist(NrmImg(:,:,2), 0, edges);
+% %     HistB = dohist(NrmImg(:,:,3), 0, edges);
+% %     SmoothHistR = conv(filter,HistR);
+% %     SmoothHistG = conv(filter,HistG);
+% %     SmoothHistB = conv(filter,HistB);
+% %     TImg = NrmImg;
+% %     for j = 1:3
+% %         TImg(:,:,j) = (NrmImg(:,:,j) > 0.45);
+% %     end
+%     
+%     clf
+%     
+% %     subplot(2,1,1);
+% %     imshow(TImg);
+% %     subplot(2,1,2);
+% %     plot(edges, HistR, 'r', edges, HistG, 'g', edges, HistB, 'b');
+% %     axis([0, 1, 0, 1.1*max(HistR)]);
+%     
+%     pause(0.1);
+% end
+% 
 
--- normalize_rgb.m
+++ normalize_rgb.m
@@ -1,6 +1,13 @@
 function [OutImg] = normalize_rgb(Img)
     OutImg = zeros(size(Img));
-    SumImg = sum(Img, 3);
+    % SumImg = sum(Img, 3);
+    [H,W,C] = size(Img);
+    SumImg = zeros(H, W);
+    ColW = [0.3 0.59 0.11] * 2;
+    % ColW = [1.0 1.0 1.0];
+    for i = 1:3
+        SumImg = SumImg + Img(:,:,i) .* ColW(i);
+    end
     for i = 1:3
         OutImg(:,:,i) = Img(:,:,i) ./ SumImg;
     end
