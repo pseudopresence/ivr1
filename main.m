5f9f67302533df46fe1ba00c5cd6ab68f495c054
mankmanky1 <mankmanky1@gmail.com>
2011-10-19 00:38:08 +0100

    Experimentation

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 clear all
 
-ImgData = myreadfolder('data2/', 100);
+ImgData = myreadfolder('data1/',10);
 
 % compute the average for each pixel
 % average = zeros(size(test_img));
@@ -11,27 +11,40 @@ ImgData = myreadfolder('data2/', 100);
 %imshow(average);
 
 % compute the median for each pixel
-% med =  median(input, 4);
-% med =  median(input(:,:,:,60:100), 4);
-% med =  median(ImgData(:,:,:,1:10:100), 4);
-% imshow(med);
+%med =  median(input, 4);
+%med =  median(input(:,:,:,60:100), 4);
+%med =  median(ImgData(:,:,:,1:40), 4);
+%imshow(med);
+%****************************************************
+
+%****************************************************
+
 
 % difference between each image and the median
 % for i = 1:100
 %     imagesc((ImgData(:,:,:,i) - med)/2 + 0.5);
 % end
 
-% adaptive thresholding
-Img = ImgData(:,:,:,5);
+% Normalising the image
+Img = ImgData(:,:,:,9);
 NImg = normalize_rgb(Img);
 AdtImg = zeros(size(NImg));
-str = strel(ones(3,3));
+
+
+
+
+%str = strel(ones(3,3));
 for i = 1:3
     % AdtImg(:,:,i) = imclose(imopen(adapt(Img(:,:,i), 45, 0.3), str), str);
     % AdtImg(:,:,i) = adapt(NImg(:,:,i), 45, 0.05);
 end
 % imshow(AdtImg);
+subplot(3,2,1)
 imshow(NImg);
+
+
+%********************************************************
+smoothingparam = 20;
 
 %Test with a single histogram
 edges = zeros(256,1);
@@ -39,8 +52,35 @@ for i = 1 : 256;
      edges(i) = (i-1)/255.0;
 end
 HistR = dohist(NImg(:,:,1), 0, edges);
+HistG = dohist(NImg(:,:,2), 0, edges);
+HistB = dohist(NImg(:,:,3), 0, edges);
 
-thresholdR = findthresh(HistR, 5/255, 4);
+subplot(3,2,2);
+plot(edges, HistR, 'r', edges, HistG, 'g', edges, HistB, 'b');
+axis([0.3, 0.38, 0, 1.5*max(HistB)]);
+
+thresholdR = myfindthresh(HistR, smoothingparam, 0)/255
+thresholdG = myfindthresh(HistG, smoothingparam, 0)/255
+thresholdB = myfindthresh(HistB, smoothingparam, 0)/255
+
+str = strel(ones(3,3));
+
+TImg(:,:,1) = (NImg(:,:,1) < 0.35);
+TImg(:,:,2) = (NImg(:,:,2) < 0.35);
+TImg(:,:,3) = (NImg(:,:,3) < 0.35);
+
+%size(TImg(:,:,3))
+subplot(3,2,3)
+imshow(TImg(:,:,1))
+xlabel('Red Channel')
+subplot(3,2,4)
+imshow(TImg(:,:,2))
+xlabel('Green Channel')
+subplot(3,2,5)
+imshow(TImg(:,:,3))
+xlabel('Blue Channel')
+%********************************************************8
+
 % filter = fspecial('gaussian', [50 1], 6);
 % filter = filter/sum(filter);  % this normalises the filter
 % edges = zeros(256,1);
--- normalize_rgb.m
+++ normalize_rgb.m
@@ -3,8 +3,8 @@ function [OutImg] = normalize_rgb(Img)
     % SumImg = sum(Img, 3);
     [H,W,C] = size(Img);
     SumImg = zeros(H, W);
-    ColW = [0.3 0.59 0.11] * 2;
-    % ColW = [1.0 1.0 1.0];
+    %ColW = [0.3 0.59 0.11] * 2;
+     ColW = [1.0 1.0 1.0];
     for i = 1:3
         SumImg = SumImg + Img(:,:,i) .* ColW(i);
     end
