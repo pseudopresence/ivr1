a3dc04f4a074303318dff060cc7b615a6c1acc85
Chris Swetenham <cswetenham@gmail.com>
2011-10-28 15:41:36 +0100

    Dan's code for centroids, direction, bounding box

---
+++ calcCenterMass.m
@@ -0,0 +1,52 @@
+function centerMassOut = calcCenterMass(TImgR, TImgG, TImgB)
+
+%Calculate the area of the object in the binary image for each channel
+    [rows,cols,valsR] = find(TImgR==1);
+    imageAreaR = sum(valsR);
+    [rows,cols,valsG] = find(TImgG==1);
+    imageAreaG = sum(valsG);
+    [rows,cols,valsB] = find(TImgB==1);
+    imageAreaB = sum(valsB);
+    
+    %Calculate the center of mass for image
+    centerMass=zeros(3,2);
+    for r=1:size(TImgR,1)
+        for c=1:size(TImgR,2)
+           centerMass(1,1) = centerMass(1,1)+r*TImgR(r,c);
+           centerMass(1,2) = centerMass(1,2)+c*TImgR(r,c);
+        end
+    end
+    
+     for r=1:size(TImgG,1)
+        for c=1:size(TImgG,2)
+        centerMass(2,1) = centerMass(2,1)+r*TImgG(r,c);
+           centerMass(2,2) = centerMass(2,2)+c*TImgG(r,c);
+        end
+     end
+    
+      for r=1:size(TImgB,1)
+        for c=1:size(TImgB,2)
+         centerMass(3,1) = centerMass(3,1)+r*TImgB(r,c);
+           centerMass(3,2) =  centerMass(3,2)+c*TImgB(r,c);
+        end
+    end
+    
+     
+          
+    
+    %Center of Mass output
+    centerMassOut = zeros(3,2);
+    %rcenterR
+    centerMassOut(1,1)= centerMass(1,1)/imageAreaR;
+    %ccenterR
+    centerMassOut(1,2)= centerMass(1,2)/imageAreaR;
+    
+    %rcenterG 
+    centerMassOut(2,1)= centerMass(2,1)/imageAreaG;
+    %ccenterG
+    centerMassOut(2,2)= centerMass(2,2)/imageAreaG;
+    
+    %rcenterB
+    centerMassOut(3,1)= centerMass(3,1)/imageAreaB;
+    %ccenterB
+    centerMassOut(3,2)= centerMass(3,2)/imageAreaB;
--- main.m
+++ main.m
@@ -1,9 +1,22 @@
+clc
 clear all
 clc
 
-ImgData = myreadfolder('data9/', 100);
 
-for k = 1:100
+ImgData = myreadfolder('data2/', 60);
+
+medImg =  median(ImgData(:,:,:,[5 55 60]), 4);
+
+%****************************************************
+%The variables used to link the objects movement
+xLinkerR = [];
+yLinkerR=[];
+xLinkerG = [];
+yLinkerG=[];
+xLinkerB = [];
+yLinkerB=[];
+
+for k = 5:5:60
     filter = fspecial('gaussian', [5 5], 5);
     
     Img = ImgData(:,:,:,k);
@@ -31,44 +44,103 @@ for k = 1:100
     [CG, ThreshG] = xyhistmax(ImgG);
     [CB, ThreshB] = xyhistmax(ImgB);
     
+
     clf();
 
     BBSize = 120;
 
-    subplot(3,3,1:6);
-    hold on;
-    imshow(NNImg);
-    plot(CR(2),CR(1),'o');
-    rectangle('Position', [CR(2) - BBSize/2, CR(1) - BBSize/2, BBSize, BBSize]);
-    plot(CG(2),CG(1),'o');
-    rectangle('Position', [CG(2) - BBSize/2, CG(1) - BBSize/2, BBSize, BBSize]);
-    plot(CB(2),CB(1),'o');
-    rectangle('Position', [CB(2) - BBSize/2, CB(1) - BBSize/2, BBSize, BBSize]);
-    hold off;
+    
+%     imshow(NNImg);
+%     plot(CR(2),CR(1),'o');
+%     rectangle('Position', [CR(2) - BBSize/2, CR(1) - BBSize/2, BBSize, BBSize]);
+%     plot(CG(2),CG(1),'o');
+%     rectangle('Position', [CG(2) - BBSize/2, CG(1) - BBSize/2, BBSize, BBSize]);
+%     plot(CB(2),CB(1),'o');
+%     rectangle('Position', [CB(2) - BBSize/2, CB(1) - BBSize/2, BBSize, BBSize]);
+%     hold off;
     xlabel(int2str(k));
     
-    TImgR = cliprect(ImgR, CR, BBSize);
-    TImgG = cliprect(ImgG, CG, BBSize);
-    TImgB = cliprect(ImgB, CB, BBSize);
+    TImgR = cliprect(ImgR, CR, BBSize)>ThreshR;
+    TImgG = cliprect(ImgG, CG, BBSize)>ThreshG;
+    TImgB = cliprect(ImgB, CB, BBSize)>ThreshB;
+    
+    %To determine the center of mass of the objects
+    %***********************************
+    
+    centerMass = calcCenterMass(TImgR, TImgG, TImgB);
+   
+    
+    %******************************************************
+    %Calculate and display the bounding box for the image
+
+    [verticesXR, verticesYR, centroidR] = calcBoundingBox(TImgR);
+    [verticesXG, verticesYG, centroidG] = calcBoundingBox(TImgG);
+    [verticesXB, verticesYB, centroidB] = calcBoundingBox(TImgB);
+    
+    
+    centrRX = centroidR(1)
+    centrRY = centroidR(2)
+    %******************************************************
+    %Calculate the orientation for each robot
+    [centerMassXR,centerMassYR] = calcBoundingBoxCM(verticesXR, verticesYR, TImgR)
+    [centerMassXG,centerMassYG] = calcBoundingBoxCM(verticesXG, verticesYG, TImgG)
+    [centerMassXB,centerMassYB] = calcBoundingBoxCM(verticesXB, verticesYB, TImgB)
+    
+   %************************************************
+   %To link the points on the estimated background image
+   xLinkerR = [xLinkerR (centerMass(1,2) + CR(2) - BBSize/2)];
+   yLinkerR = [yLinkerR (centerMass(1,1) + CR(1) - BBSize/2)];
+   xLinkerG = [xLinkerG (centerMass(2,2) + CG(2) - BBSize/2)];
+   yLinkerG = [yLinkerG (centerMass(2,1) + CG(1) - BBSize/2)];
+   xLinkerB = [xLinkerB (centerMass(3,2) + CB(2) - BBSize/2)];
+   yLinkerB = [yLinkerB (centerMass(3,1) + CB(1) - BBSize/2)];
+   
+   %Define unit vectors in the direction of the centroid
+   centerMassR = [centerMassXR centerMassYR];
+   dR = (centroidR - centerMassR);
+   
+   centerMassG = [centerMassXG centerMassYG];
+   dG = (centroidG - centerMassG);
+   
+   centerMassB = [centerMassXB centerMassYB];
+   dB = (centroidB - centerMassB);
+   
+   %The unit vectors for each channel
+   dR = dR/norm(dR);
+   dG = dG/norm(dG);
+   dB = dB/norm(dB);
+   
+    subplot(3,3,1:6);
+    imshow(NNImg);
+    hold on
+    plot(xLinkerR, yLinkerR, 'xr-', xLinkerG, yLinkerG, 'xg-', xLinkerB, yLinkerB, 'xb-');
+    xlabel(k);
         
     subplot(3,3,7);
     hold on;
-    imshow(TImgR > ThreshR);
-    hold off;
+    imshow(TImgR);
+    plot(verticesXR, verticesYR, 'r-', 'LineWidth', 2);
+    hold on
+    plot([centerMassR(1),centroidR(1)+30*dR(1)], [centerMassR(2), centroidR(2)+30*dR(2)],'b-', 'LineWidth',2);
     xlabel('red');
     
     subplot(3,3,8);
     hold on;
-    imshow(TImgG > ThreshG);
-    hold off;
+    imshow(TImgG);
+    hold on
+    plot(verticesXG, verticesYG, 'g-', 'LineWidth', 2);
+    hold on
+    plot([centerMassG(1),centroidG(1)+30*dG(1)], [centerMassG(2), centroidG(2)+30*dG(2)],'b-', 'LineWidth',2);
     xlabel('green');
     
     subplot(3,3,9);
     hold on;
-    imshow(TImgB > ThreshB);
-    hold off;
+    imshow(TImgB);
+    hold on
+    plot(verticesXB, verticesYB, 'b-', 'LineWidth', 2);
+    hold on
+    plot([centerMassB(1),centroidB(1)+30*dB(1)], [centerMassB(2), centroidB(2)+30*dB(2)],'b-', 'LineWidth',2);
     xlabel('blue');
     
     pause(0.1);
 end
-
