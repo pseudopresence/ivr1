326e5cb985b6ea8eacbe7e7f2fc669e4e0e4e74f
Daniel Mankowitz <daniel@mankowitz.co.za>
2011-11-01 20:14:20 +0000

    Arrows now added to robots and displayed in main image

--- main.m
+++ main.m
@@ -4,7 +4,7 @@ clc;
 clf;
 
 % Configuration section
-DATA_FOLDER = 'data7/';
+DATA_FOLDER = 'data2/';
 ENABLE_TOOLBOX = 1;
 MAX_IMG_COUNT = 100;
 FILTER_SIZE = 5;
@@ -19,7 +19,7 @@ HaveToolbox = ENABLE_TOOLBOX && license('checkout', 'Image_Toolbox');
 ImgData = myreadfolder(DATA_FOLDER, MAX_IMG_COUNT);
 
 % Compute the median image
-MedianIndices = [5 75 100];
+MedianIndices = [5 75 80];
 MedianImgs = zeros(size(ImgData,1),    ...
                    size(ImgData,2),    ...
                    size(ImgData,3),    ...
@@ -47,6 +47,8 @@ for Idx = 1:size(MedianIndices,2)
 end
 
 MedImg =  median(MedianImgs, 4);
+
+imshow(MedImg);
 
 OldDirR = [0 0];
 OldDirG = [0 0];
@@ -110,6 +112,15 @@ for ImgIdx = 5:5:MAX_IMG_COUNT
     trueCMXB = (CenterMassXB + CB(2) - BB_SIZE/2);
     trueCMYB = (CenterMassYB + CB(1) - BB_SIZE/2);
     
+    %Store the true position of the centroid of the 
+    %bounding box of the images
+    trueCentroidBBXR = (CentroidR(1) + CR(2) - BB_SIZE/2);
+    trueCentroidBBYR = (CentroidR(2) + CR(1) - BB_SIZE/2);
+    trueCentroidBBXG = (CentroidG(1) + CG(2) - BB_SIZE/2);
+    trueCentroidBBYG = (CentroidG(2) + CG(1) - BB_SIZE/2);
+    trueCentroidBBXB = (CentroidB(1) + CB(2) - BB_SIZE/2);
+    trueCentroidBBYB = (CentroidB(2) + CB(1) - BB_SIZE/2);
+    
     %Store the previous Center of mass of the objects
     %for plotting purposes
     
@@ -137,18 +148,27 @@ for ImgIdx = 5:5:MAX_IMG_COUNT
     DG = DG/norm(DG);
     DB = DB/norm(DB);
    
+    %Colour for the orientation arrows in the bouding box
     LineColR = 'b-';
     LineColG = 'b-';
     LineColB = 'b-';
     
+    %Colour for the orientation arrows on the main image
+    LineColRArrow = [0 0 1];
+    LineColGArrow = [0 0 1];
+    LineColBArrow = [0 0 1];
+    
     if dot(DR, OldDirR) < 0.5
         LineColR = 'r-';
+        LineColRArrow = [1 0 0];
     end
     if dot(DG, OldDirG) < 0.5
         LineColG = 'r-';
+        LineColGArrow = [1 0 0];
     end
     if dot(DB, OldDirB) < 0.5
         LineColB = 'r-';
+        LineColBArrow = [1 0 0];
     end
     OldDirR = DR;
     OldDirG = DG;
@@ -163,37 +183,51 @@ for ImgIdx = 5:5:MAX_IMG_COUNT
     hold on
     plot(XLinkerR, YLinkerR, 'xr-', XLinkerG, YLinkerG, 'xg-', XLinkerB, YLinkerB, 'xb-');
     xlabel(ImgIdx);
-%     myimshow(Img);
-%     colormap('default');
-%     hold on;
-%     plot([CenterMassR(1),CentroidR(1)+30*DR(1)], [CenterMassR(2), CentroidR(2)+30*DR(2)], LineColR, 'LineWidth',2);
-%     hold on
-%     plot([CenterMassG(1),CentroidG(1)+30*DG(1)], [CenterMassG(2), CentroidG(2)+30*DG(2)], LineColG, 'LineWidth',2);
-%     hold on
-%     plot([CenterMassB(1),CentroidB(1)+30*DB(1)], [CenterMassB(2), CentroidB(2)+30*DB(2)], LineColB, 'LineWidth',2);
-%     
+    myimshow(Img);
+    colormap('default');
+    hold on;
+    %Plot the arrow on each of the cars to indicate their
+    %orientation
+    plot_arrow(trueCMXR,trueCMYR, trueCentroidBBXR+30*DR(1),trueCentroidBBYR+30*DR(2),'linewidth',2,'headwidth',0.25,'headheight',0.33,'color',LineColRArrow,'facecolor',LineColRArrow);
+    %plot([trueCMXR,trueCentroidBBXR+30*DR(1)], [trueCMYR, trueCentroidBBYR+30*DR(2)], LineColR, 'LineWidth',2);
+    hold on
+    plot_arrow(trueCMXG,trueCMYG, trueCentroidBBXG+30*DR(1),trueCentroidBBYG+30*DR(2),'linewidth',2,'headwidth',0.25,'headheight',0.33,'color',LineColGArrow,'facecolor',LineColGArrow);
+    %plot([trueCMXG,trueCentroidBBXG+30*DG(1)], [trueCMYG, trueCentroidBBYG+30*DG(2)], LineColG, 'LineWidth',2);
+    hold on
+    plot_arrow(trueCMXB,trueCMYB, trueCentroidBBXB+30*DR(1),trueCentroidBBYB+30*DR(2),'linewidth',2,'headwidth',0.25,'headheight',0.33,'color',LineColBArrow,'facecolor',LineColBArrow);
+    %plot([trueCMXB,trueCentroidBBXB+30*DB(1)], [trueCMYB, trueCentroidBBYB+30*DB(2)], LineColB, 'LineWidth',2);
+    
+    %Plot the red channel with its corresponding bounding
+    %box
     subplot(3,3,7);
     myimshow(TImgR);
     colormap('gray');
     hold on;
     plot(VerticesXR, VerticesYR, 'r-', 'LineWidth', 5);
-    plot([CenterMassR(1),CentroidR(1)+30*DR(1)], [CenterMassR(2), CentroidR(2)+30*DR(2)], LineColR, 'LineWidth',2);
+    plot_arrow(CenterMassR(1),CenterMassR(2), CentroidR(1)+30*DR(1),CentroidR(2)+30*DR(2),'linewidth',2,'headwidth',0.25,'headheight',0.33,'color',LineColRArrow,'facecolor',LineColRArrow);
+    %plot([CenterMassR(1),CentroidR(1)+30*DR(1)], [CenterMassR(2), CentroidR(2)+30*DR(2)], LineColR, 'LineWidth',2);
     xlabel('Red Channel');
 
+    %Plot the green channel with its corresponding bounding
+    %box
     subplot(3,3,8);
     myimshow(TImgG);
     colormap('gray');
     hold on;
     plot(VerticesXG, VerticesYG, 'g-', 'LineWidth', 5);
-    plot([CenterMassG(1),CentroidG(1)+30*DG(1)], [CenterMassG(2), CentroidG(2)+30*DG(2)], LineColG, 'LineWidth',2);
+    plot_arrow(CenterMassG(1),CenterMassG(2), CentroidG(1)+30*DR(1),CentroidG(2)+30*DR(2),'linewidth',2,'headwidth',0.25,'headheight',0.33,'color',LineColRArrow,'facecolor',LineColRArrow);
+    %plot([CenterMassG(1),CentroidG(1)+30*DG(1)], [CenterMassG(2), CentroidG(2)+30*DG(2)], LineColG, 'LineWidth',2);
     xlabel('Green Channel');
 
+    %Plot the blue channel with its corresponding bounding
+    %box
     subplot(3,3,9);
     myimshow(TImgB);
     colormap('gray');
     hold on;
     plot(VerticesXB, VerticesYB, 'b-', 'LineWidth', 5);
-    plot([CenterMassB(1),CentroidB(1)+30*DB(1)], [CenterMassB(2), CentroidB(2)+30*DB(2)], LineColB, 'LineWidth',2);
+    plot_arrow(CenterMassB(1),CenterMassB(2), CentroidB(1)+30*DR(1),CentroidB(2)+30*DR(2),'linewidth',2,'headwidth',0.25,'headheight',0.33,'color',LineColRArrow,'facecolor',LineColRArrow);
+    %plot([CenterMassB(1),CentroidB(1)+30*DB(1)], [CenterMassB(2), CentroidB(2)+30*DB(2)], LineColB, 'LineWidth',2);
     xlabel('Blue Channel');
 
     pause(0.1);
